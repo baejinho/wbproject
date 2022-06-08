96653ef999d3e9573a2bb9554370c1dee19b354f
baejinho <baeno@nate.com>
2022-06-08 21:01:20 +0900

    lunch card

--- src/pages/Day2Cardview.jsx
+++ src/pages/Day2Cardview.jsx
@@ -21,7 +21,7 @@ import {
 
 
 import { EditOutlined, EllipsisOutlined, SettingOutlined
-,DeleteOutlined } from '@ant-design/icons';
+,DeleteOutlined, LikeOutlined } from '@ant-design/icons';
  
 
 import 'antd/dist/antd.css';
@@ -49,9 +49,9 @@ export default class Day2Cardview extends Component {
 
   load=()=>{
     let menuList = [];
-    menuList.push({lunchAmount:4000,lunchName:'치킨', lunchLocation:'https://map.naver.com/v5/search/%EC%B9%98%ED%82%A8%EC%A7%91%EC%97%AD%EC%82%BC/place/35734815?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&placeSearchOption=entry=pll%26fromNxList=true'})
-    menuList.push({lunchAmount:8000,lunchName:'프리모바치오바치 강남점', lunchLocation:'https://map.naver.com/v5/search/%ED%94%BC%EC%9E%90/place/18407600?c=14139657.4259043,4509008.2411027,15,0,0,0,dh'})
-    menuList.push({lunchAmount:4000,lunchName:'사람사는 고깃집 김일도', lunchLocation:' https://map.naver.com/v5/search/%EA%B0%95%EB%82%A8%EB%A7%9B%EC%A7%91/place/1972037992?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&placeSearchOption=entry=pll%26fromNxList=true'})
+    menuList.push({lunchAmount:4000,lunchName:'치킨', lunchCount :0,lunchLocation:'https://map.naver.com/v5/search/%EC%B9%98%ED%82%A8%EC%A7%91%EC%97%AD%EC%82%BC/place/35734815?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&placeSearchOption=entry=pll%26fromNxList=true'})
+    menuList.push({lunchAmount:8000,lunchName:'프리모바치오바치 강남점',lunchCount :0, lunchLocation:'https://map.naver.com/v5/search/%ED%94%BC%EC%9E%90/place/18407600?c=14139657.4259043,4509008.2411027,15,0,0,0,dh'})
+    menuList.push({lunchAmount:4000,lunchName:'사람사는 고깃집 김일도',lunchCount :0, lunchLocation:' https://map.naver.com/v5/search/%EA%B0%95%EB%82%A8%EB%A7%9B%EC%A7%91/place/1972037992?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&placeSearchOption=entry=pll%26fromNxList=true'})
 
 
     this.setState({menuList});
@@ -116,7 +116,8 @@ export default class Day2Cardview extends Component {
               let menuList = this.state.menuList;
 
               menuList.push({lunchName:this.state.lunchName,
-              lunchAmount:this.state.lunchAmount});
+              lunchAmount:this.state.lunchAmount,
+              lunchCount :0});
 
               this.setState({menuList, lunchName:"", lunchAmount:0,
               isProductModal:false})
@@ -213,10 +214,18 @@ export default class Day2Cardview extends Component {
           return <Card
           style={{ width: 300, marginLeft:5 }}
           cover={
-            <img
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
+            <div  style={{position:"relative"}}>
+              
+              <div style={{position:"relative"}}>
+              <img
+                style={{width:"100%"}}
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+              </div>
+              <div style={{position:"absolute", right:10, top:10}}>{item.lunchCount}</div>
+            
+            </div>
           }
           actions={[
             <DeleteOutlined key="delete"
@@ -238,7 +247,20 @@ export default class Day2Cardview extends Component {
                   lunchAmount:item.lunchAmount})
               }}
             />,
-            <EllipsisOutlined key="ellipsis" />,
+            <LikeOutlined key="like"
+            
+            onClick={()=>{
+               
+              let menuList = this.state.menuList;
+              let menuObj = menuList[index];
+              console.log(menuObj);
+              menuObj.lunchCount = menuObj.lunchCount+1; 
+              
+              menuList[index] = menuObj;
+
+              this.setState({menuList})
+              }}
+             />,
           ]}
         >
           <Meta
